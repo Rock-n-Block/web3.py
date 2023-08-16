df2321a4a8811ecbc262fd7ad5ec76ae7ae49931
joxerx <n.lebedinsky@bk.ru>
2023-08-16 18:46:10 +0300

    Downgrade eth-abi

--- setup.py
+++ setup.py
@@ -70,7 +70,7 @@ setup(
     include_package_data=True,
     install_requires=[
         "aiohttp>=3.7.4.post0",
-        "eth-abi>=3.0.0",
+        "eth-abi>=2.0.0",
         "eth-account>=0.4.0",
         "eth-hash[pycryptodome]>=0.5.1",
         "eth-typing>=2.2.2",
